151794c75745ca737e3d2196955db912c911858c
Ivan <62833875+KeilWin@users.noreply.github.com>
2025-09-29 01:03:09 +0300

    Update docker_run.sh

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,1 +1,1 @@
-docker run -d -v ./ssl:/etc/ssl -v ./stats:/app/stats -v ./logs:/app/logs -p 443:8080 --name httpinfo-dev strumeg/httpinfo:latest
+docker run -d -v ./ssl:/etc/ssl -v ./stats:/app/stats -v ./logs:/app/logs -p 443:8080 --name httpinfo-dev $DOCKER_DEV_IMAGE_NAME
